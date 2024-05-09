8768a57061a4e1a01fa54bfc382259ffef19f8e0
TinTakalic <119415890+TinTakalic@users.noreply.github.com>
2024-05-09 15:58:06 +0200

    Initial commit

---
+++ app/src/main/java/com/elpros/glagoljicatakalic/MainActivity.java
@@ -0,0 +1,28 @@
+package com.elpros.glagoljicatakalic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText et = findViewById(R.id.editTextTextMultiLine);
+        TextView tw = findViewById(R.id.textView3);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tw.setText(et.getText());
+            }
+        });
+    }
+}
